9f718526ff514c5877ae7bd9c1c874e7b5fc6d63
Daniel Nash <dnl.nash@gmail.com>
2026-08-21 22:37:58 -0400

    Container builder: Remove hooks from host and do not perform overwrite during upgrades

--- make-rootfs.sh
+++ make-rootfs.sh
@@ -221,11 +221,11 @@ export dockerplat="--platform linux/amd64"
 export dockerplat="--platform linux/"${arch}
 ;;
 esac
-rm -rf "${work_dir}/${arch}/airootfs/etc/pacman.d/gnupg"
+rm -rf "${work_dir}/${arch}/airootfs/etc/pacman.d/gnupg" "${work_dir}/${arch}/airootfs//etc/pacman.d/hooks"
 cat >> dockerfile.`echo -en $dockerplat|sed "s|--platform linux\/||g"|tr / +`<<EOF
 FROM scratch
 COPY "${work_dir}/${arch}/airootfs/" /
-ONBUILD RUN pacman-key --init&&pacman-key --populate&&pacman --needed --noconfirm --overwrite \* -Syu
+ONBUILD RUN pacman-key --init&&pacman-key --populate&&pacman --noconfirm -Syu
 EOF
 docker build --tag $jenux_iso_docker_repo":"jenux-${preset}-${arch} $dockerplat . -f dockerfile.`echo -en $dockerplat|sed "s|--platform linux\/||g"|tr / +`
 docker push $jenux_iso_docker_repo":"jenux-${preset}-${arch} `echo -en $dockerplat`
